76657d75885ac42da00bdf5216ea1ece78620ded
jenia996 <jenian996@gmail.com>
2016-03-18 21:45:02 +0300

    First Version

---
+++ GameCatalog/Game.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameCatalog
+{
+    class Game
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public double averageMark { get; set; }
+        public Genre genre { get; set; }
+        public string dateAdded { get; set; }
+        public string image { get; set; }
+        public string description { get; set; }
+        public Platform platform { get; set; }
+
+
+    }
+}
---
+++ GameCatalog/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GameCatalog
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private List<Game> initialize()
+        {
+            List<Game> games = new List<Game>();
+            games.Add(new Game() { id = 1, name = "Mass Effect", description = "Some Description", averageMark = 9.5, image = @"..\..\Data\Mass Effect.jpg", dateAdded = DateTime.Now.ToString("yyyy/MM/dd"), genre = Genre.RPG });
+            games.Add(new Game() { id = 2, name = "Mass effect 2", dateAdded = DateTime.Now.ToString("yyyy/MM/dd"), genre = Genre.Action, averageMark = 9.1, image = @"..\..\Data\Mass Effect 2.jpg" });
+            return games;
+        }
+        public MainWindow()
+        {
+            InitializeComponent();
+            dataGrid.ItemsSource = initialize();
+        }
+
+
+        private void dataGrid_SelectionChanged_1(object sender, SelectionChangedEventArgs e)
+        {
+            Game game = (Game)dataGrid.SelectedItem;
+            String path = System.IO.Path.GetFullPath(game.image);
+            if (System.IO.File.Exists(path))
+            {
+                ImageSource imageSource = new BitmapImage(new Uri(path));
+                image.Source = imageSource;
+            }
+            description.Content = game.description;
+        }
+    }
+}
